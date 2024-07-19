5c2565a101870244b457ee2fea0c2632b3007046
Xinyue (Luna) <56839374+Summer-luna@users.noreply.github.com>
2024-07-19 11:47:03 -0500

    Update docker_install.sh

--- cn/docker_install.sh
+++ cn/docker_install.sh
@@ -1,2 +1,2 @@
-sudo curl -fsSL get.docker.com -o get-docker.sh
+sudo curl https://raw.githubusercontent.com/Summer-luna/Installation_Scripts/main/cn/get-docker.sh -o get-docker.sh
 sudo sh get-docker.sh --mirror Aliyun
